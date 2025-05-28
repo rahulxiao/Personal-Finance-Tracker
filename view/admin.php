90f78cd9958147713e9a9522036fd1402c2cec73
rAHUL <rahulbiswasaiub@gmail.com>
2025-05-29 02:16:45 +0600

    v6.0

--- view/admin.php
+++ view/admin.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    if(isset($_SESSION['status'])){
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -530,3 +534,11 @@
 </body>
 
 </html>
+
+
+<?php
+    }else{
+        header('location: login.php');
+    }
+
+?>
